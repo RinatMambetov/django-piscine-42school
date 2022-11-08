406595ec0605fad9f7f36c2388e69091366c622b
rinat <astrakhan.rinat@gmail.com>
2022-11-08 11:06:57 +0300

    added description

--- zappy/todowoo/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Todo
-
-
-class TodoAdmin(admin.ModelAdmin):
-	readonly_fields = ('created',)	#для отображения в админке ридонли полей
-
-
-admin.site.register(Todo, TodoAdmin)
